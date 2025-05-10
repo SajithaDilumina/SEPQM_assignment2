a280a3b798cabf68bce58d2af924145144ec8697
SajithaDilumina <156515691+SajithaDilumina@users.noreply.github.com>
2025-05-10 14:08:37 +0530

    sajitha

--- siluni/cypress/e2e/TC_08_test.cy.js
+++ siluni/cypress/e2e/TC_08_test.cy.js
@@ -1,30 +1,33 @@
-describe('TC_08_PIM - Search for Employee in PIM Section', () => {
-    it('should return correct employee details in PIM search results', () => {
-      // Step 1: Login
-      cy.visit('https://opensource-demo.orangehrmlive.com/');
-      cy.get('input[name="username"]').type('Admin');
-      cy.get('input[name="password"]').type('admin123');
-      cy.get('button[type="submit"]').click();
-  
-      // Step 2: Navigate to PIM > Employee List
-      cy.contains('PIM').click();
-      cy.url().should('include', '/pim/viewEmployeeList');
-  
-      // Step 3: Fill Search Filters
-      cy.get('input[placeholder="Type for hints..."]').eq(0).type('Ava');
-  
-      // Employment Status
-      cy.get('.oxd-select-text').eq(0).click();
-      cy.get('.oxd-select-dropdown').contains('Full-Time Probation').click();
-  
-      // Step 4: Click Search
-      cy.get('button[type="submit"]').contains('Search').click();
-  
-      // Step 5: Validate results for Ava
-      cy.get('.oxd-table-body', { timeout: 10000 }).should('be.visible').within(() => {
-        cy.contains('Ava').should('exist');
-        cy.get('div').eq(5).should('contain.text', 'Full-Time Probation');
-      });
-    });
+describe("PIM - Employee Search Functionality", () => {
+  beforeEach(() => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.get('input[name="username"]').type("Admin");
+    cy.get('input[name="password"]').type("admin123");
+    cy.get('button[type="submit"]').click();
+    cy.url().should("include", "/dashboard");
+
+    // Navigate to PIM section
+    cy.contains("PIM").click();
+    cy.url().should("include", "/pim/viewEmployeeList");
   });
-  
+
+  it("TC_08: Should return correct employee details in PIM search results", () => {
+    cy.get('input[placeholder="Type for hints..."]').first().type("Joy");
+    cy.get('button[type="submit"]').contains("Search").click();
+
+    cy.get(".oxd-table-body", { timeout: 10000 })
+      .should("be.visible")
+      .within(() => {
+        cy.contains(".oxd-table-cell", "Joy").should("exist");
+      });
+  });
+
+  it("TC_09: Should return no results when searching for a non-existent employee", () => {
+    cy.get('input[placeholder="Type for hints..."]')
+      .first()
+      .type("IamNotAnEmployee");
+    cy.get('button[type="submit"]').contains("Search").click();
+
+    cy.get(".oxd-table-body").find(".oxd-table-row").should("have.length", 0);
+  });
+});
